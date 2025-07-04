2f054a4571316270805b47ad753bebebb931eb96
Bzver <t.c.pedrovsky@gmail.com>
2025-07-04 20:50:20 +0800

    Relocate sleap_keypoint_fill script and update keypoint_moseq script to accept 3D dannce format

--- behavioral_processing/KPMS/project_creation.py
+++ behavioral_processing/KPMS/project_creation.py
@@ -1,23 +1,71 @@
 import keypoint_moseq as kpms
 import re
 
-project_dir = "/home/pedrov/kpms_project"
-config = lambda: kpms.load_config(project_dir)
+project_dir = "/home/bezver/project/SD-20250714"
 
-sleap_file = "/mnt/d/Project/deepof/Tables/20250117-OFT-test1-toe1-con-conv.analysis.h5"  # any .slp or .h5 file with predictions for a single video
-kpms.setup_project(project_dir, sleap_file=sleap_file)
+# # Sleap setup
+# sleap_file = "/mnt/d/Project/deepof/Tables/20250117-OFT-test1-toe1-con-conv.analysis.h5"  # any .slp or .h5 file with predictions for a single video
+# kpms.setup_project(project_dir, sleap_file=sleap_file)
+
+# Custom Setup ( for 3D )
+bodyparts = [
+    "EarL",
+    "EarR",
+    "Snout",
+    "SpineF",
+    "SpineM",
+    "Tail(base)",
+    "Tail(mid)",
+    "Tail(end)",
+    "ForePawL",
+    "ForeLimbL",
+    "ForePawR",
+    "ForeLimbR",
+    "HindPawL",
+    "HindLimbL",
+    "HindPawR",
+    "HindLimbR",
+]
+
+skeleton = [
+    ["EarL", "EarR"],
+    ["EarR", "Snout"],
+    ["EarL", "Snout"],
+    ["EarL", "SpineM"],
+    ["SpineM", "Tail(base)"],
+    ["Tail(base)", "Tail(mid)"],
+    ["Tail(mid)", "Tail(end)"],
+    ["ForePawL", "ForeLimbL"],
+    ["SpineF", "ForeLimbL"],
+    ["ForePawR", "ForeLimbR"],
+    ["ForeLimbR", "SpineF"],
+    ["HindPawL", "HindLimbL"],
+    ["HindLimbL", "Tail(base)"],
+    ["HindPawR", "HindLimbR"],
+    ["HindLimbR", "Tail(base)"],
+]
+
+video_dir = "/home/bezver/project/SD-20250714"
+
+kpms.setup_project(project_dir, video_dir=video_dir, bodyparts=bodyparts, skeleton=skeleton, overwrite=True)
 
 kpms.update_config(
     project_dir,
-    video_dir="/mnt/d/Project/deepof/Videos/",
-    anterior_bodyparts=["Nose"],
-    posterior_bodyparts=["Tail_base"],
-    use_bodyparts=['Nose', 'Left_ear', 'Right_ear', 'Tail_base', 'Spine_1', 'Center', 'Spine_2', 'Left_fhip', 'Right_fhip', 'Left_bhip', 'Right_bhip'],
+    video_dir=video_dir,
+    anterior_bodyparts=["Snout"],
+    posterior_bodyparts=["Tail(base)"],
+    use_bodyparts=['Snout', 'EarL', 'EarR', 'Tail(base)', 'SpineF', 'SpineM', '', 'Left_fhip', 'Right_fhip', 'Left_bhip', 'Right_bhip'],
 )
 
-# load data (e.g. from Sleap)
-keypoint_data_path = "/mnt/d/Project/deepof/Tables/"  # can be a file, a directory, or a list of files
-coordinates, confidences, bodyparts = kpms.load_keypoints(keypoint_data_path, "sleap")
+# # load data (e.g. from Sleap)
+# keypoint_data_path = "/mnt/d/Project/deepof/Tables/"  # can be a file, a directory, or a list of files
+# coordinates, confidences, bodyparts = kpms.load_keypoints(keypoint_data_path, "sleap")
+
+# load 3D data (e.g. from DANNCE)
+keypoint_data_path = "/home/bezver/project/SD-20250714/save_data_AVG0.mat"  # can be a file, a directory, or a list of files
+coordinates, confidences, bodyparts = kpms.load_keypoints(keypoint_data_path, "dannce")
+
+config = lambda: kpms.load_config(project_dir)
 
 # format data for modeling
 data, metadata = kpms.format_data(coordinates, confidences, **config())
@@ -29,7 +77,7 @@ kpms.save_pca(pca, project_dir)
 kpms.print_dims_to_explain_variance(pca, 0.9)
 kpms.plot_scree(pca, project_dir=project_dir)
 kpms.plot_pcs(pca, project_dir=project_dir, **config())
-# use the following to load an already fit model
+# use the following to load an already fit model 
 # pca = kpms.load_pca(project_dir)
 
 latentdim = input("Num of components:")
