5ebd899e60e24d97d9dc02ddb9411ea4ceedaf7c
Bzver <t.c.pedrovsky@gmail.com>
2025-07-21 21:58:17 +0800

    Make it support prediction_only ( no annot )

--- behavioral_processing/asoid_social_preprocess.py
+++ behavioral_processing/asoid_social_preprocess.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-def asoid_preprocess(pose_file, annot_file, inst_count, fps):
+def asoid_preprocess(pose_file, annot_file=None, inst_count=2, fps=10):
     df_inst_list, df_frame, header = parse_dlc_prediction_csv(pose_file, inst_count)
     df_pose = df_frame.copy()
     indices_to_remove = set()
@@ -12,15 +12,16 @@ def asoid_preprocess(pose_file, annot_file, inst_count, fps):
         indices_to_remove.update(indices_killer)
     df_pose = df_pose.drop(indices_to_remove)
     df_pose = ensure_no_df_nan(df_pose)
-    processed_annot_path = remove_corresponding_annotation_idx(annot_file, indices_to_remove, fps)
+    if annot_file is not None:
+        processed_annot_path = remove_corresponding_annotation_idx(annot_file, indices_to_remove, fps)
+        print(f"Processed annotation file saved to: {processed_annot_path}")
     processed_pose_path = save_processed_pose(df_pose, pose_file, header)
     print(f"ASOID social preprocessing completed successfully.")
-    print(f"Processed annotation file saved to: {processed_annot_path}")
     print(f"Processed pose file saved to: {processed_pose_path}")
 
 def parse_dlc_prediction_csv(csv_path, inst_count):
     df_get_header = pd.read_csv(csv_path, header=None, low_memory=False)
-    header = df_get_header.iloc[0:4,:]
+    header = df_get_header.iloc[1:4,:] # Byebye scorer row
     df = pd.read_csv(csv_path, header=[1, 2, 3])
     df.columns = ['_'.join(col).strip() for col in df.columns.values]
     header.columns = df.columns
@@ -87,17 +88,25 @@ def save_processed_pose(df_pose, pose_file, header):
 
 if __name__ == "__main__":
     inst_count, fps = 2, 10
-    folder = "D:/Project/DLC-Models/NTD/videos/jobs/assdfa"
+    # folder = "D:/Project/DLC-Models/NTD/videos/jobs/assdfa"
     
-    pose_file_name_1 = "1-20250626C1-first3h-DDLC_HrnetW32_bezver-SD-20250605M-cam52025-06-26shuffle1_detector_090_snapshot_080_el_tr_track_refiner_modified_1.csv"
-    annot_file_name_1 = "1-20250626_annot_L.csv"
+    # pose_file_name_1 = "1-20250626C1-first3h-DDLC_HrnetW32_bezver-SD-20250605M-cam52025-06-26shuffle1_detector_090_snapshot_080_el_tr_track_refiner_modified_1.csv"
+    # annot_file_name_1 = "1-20250626_annot_L.csv"
+    # pose_file_1 = os.path.join(folder, pose_file_name_1)
+    # annot_file_1 = os.path.join(folder, annot_file_name_1)
+
+    # pose_file_name_2 = "2-20250626C1-first3h-SDLC_HrnetW32_bezver-SD-20250605M-cam52025-06-26shuffle1_detector_090_snapshot_080_el_tr_track_refiner_modified_4.csv"
+    # annot_file_name_2 = "2-20250626_annot_R.csv"
+    # pose_file_2 = os.path.join(folder, pose_file_name_2)
+    # annot_file_2 = os.path.join(folder, annot_file_name_2)
+
+    folder = "D:/Project/A-SOID/Data/20250709"
+
+    pose_file_name_1 = "1-20250709-first3h-D-convDLC_HrnetW32_bezver-SD-20250605M-cam52025-06-26shuffle1_detector_090_snapshot_080_el_tr_track_refiner_modified_0.csv"
     pose_file_1 = os.path.join(folder, pose_file_name_1)
-    annot_file_1 = os.path.join(folder, annot_file_name_1)
 
-    pose_file_name_2 = "2-20250626C1-first3h-SDLC_HrnetW32_bezver-SD-20250605M-cam52025-06-26shuffle1_detector_090_snapshot_080_el_tr_track_refiner_modified_4.csv"
-    annot_file_name_2 = "2-20250626_annot_R.csv"
+    pose_file_name_2 = "2-20250709-first3h-S-convDLC_HrnetW32_bezver-SD-20250605M-cam52025-06-26shuffle1_detector_090_snapshot_080_el_tr_track_refiner_modified_0.csv"
     pose_file_2 = os.path.join(folder, pose_file_name_2)
-    annot_file_2 = os.path.join(folder, annot_file_name_2)
 
-    asoid_preprocess(pose_file_1, annot_file_1, inst_count, fps)
-    asoid_preprocess(pose_file_2, annot_file_2, inst_count, fps)
+    asoid_preprocess(pose_file_1, None, inst_count, fps)
+    asoid_preprocess(pose_file_2, None, inst_count, fps)
